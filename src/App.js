7a84d26ce089ff936a553a8d586602f8b01ed650
Chaeee <89851362+BIONIChae@users.noreply.github.com>
2023-11-23 15:41:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Montreal" />
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              class="links"
+              href="https://chae-rich.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Chae
+            </a>{" "}
+            and{" "}
+            <a
+              class="links"
+              href="https://github.com/BIONIChae/react-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
