8fb4c4ba4c0aa9e306ea39425212d9f3a252d626
Chaeee <89851362+BIONIChae@users.noreply.github.com>
2023-11-27 20:54:18 +0200

    Create FormatDate.js

---
+++ src/FormatDate.js
@@ -0,0 +1,25 @@
+export default function FormatDate(props) {
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+  
+    const day = daysOfWeek[props.date.getDay()];
+    const date = props.date.getDate();
+    let hours = props.date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+  
+    let minutes = props.date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    return `${day} ${date}, ${hours}:${minutes}`;
+  }
