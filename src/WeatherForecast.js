d8a364f5f4e05fda172779004be5685420e885f5
Chaeee <89851362+BIONIChae@users.noreply.github.com>
2023-12-01 00:03:16 +0200

    Corrected error

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -6,39 +6,29 @@ import axios from "axios";
 export default function WeatherForecast(props) {
 
   let [loaded, setLoaded] = useState(false);
-  function handleResponse(response) {
-    console.log(response.data);
+  setForecast(response.data.daily);
+    setLoaded(true);
+
   } 
 
   if (loaded) {
+    return (
+      <div className="WeatherForecast" >
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
       let apiKey = "094780c710fa4efd669f0df8c3991927";
       let long = props.coordinates.lon;
       let lat = props.coordinates.lat;
       let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
-  
+
       axios.get(apiUrl).then(handleResponse);
-  
+
       return null;
-    } else {
-      return (
-        <div className="WeatherForecast" >
-          <div className="row">
-            <div className="col">
-              <div className="WeatherForecast-day">Tue</div>
-              <WeatherIcon code="01d" size={36} />
-              <div className="WeatherForecast-temperatures">
-                <span className="WeatherForecast-temperature-max">
-                  19°
-                </span>
-                <span className="WeatherForecast-temperature-min">
-                  10°
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      );
     }
 }
-
-
