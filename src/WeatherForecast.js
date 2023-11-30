eadc4302e356bcadc6c32eac4f36a4ed0abf0aa3
Chaeee <89851362+BIONIChae@users.noreply.github.com>
2023-11-30 13:59:02 +0200

    Updates made in  WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./WeatherForecast.css";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  
+    function handleResponse(response) {
+    console.log(response.data);
+  } 
+
+  let apiKey = "094780c710fa4efd669f0df8c3991927";
+  let long = props.coordinates.lon;
+  let lat = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast" >
       <div className="row">
